64c9ae63f90c57f58d8adf7d1b7ac40ee8ed632c
Lucas <lucas@agtlucas.com>
2016-05-02 20:17:39 -0300

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,14 @@ const gulp = require('gulp')
 const sass = require('gulp-sass')
 const gutil = require('gulp-util')
 const coffee = require('gulp-coffee')
+const concat = require('gulp-concat')
+const browserify = require('gulp-browserify')
+const coffeeify = require('coffeeify')
 
 gulp.task('coffee', () => {
-  gulp.src('./src/coffee/*.coffee')
-    .pipe(coffee({ bare: true }).on('error', gutil.log))
+  gulp.src('./src/coffee/*.coffee', { read: false })
+    .pipe(browserify({ transform: ['coffeeify'], extensions: ['.coffee'] }))
+    .pipe(concat('main.js'))
     .pipe(gulp.dest('./public/js/'))
 })
 
